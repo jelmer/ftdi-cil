5face9b53c337f604729fc7a1d93d75f43d50d11
Jelmer Vernooij <jelmer@samba.org>
2006-05-24 17:49:17 +0200

    Couple of small bugfixes, add pc file

--- ftdi-test.cs
+++ ftdi-test.cs
@@ -3,7 +3,7 @@ using System;
 
 class FTDITest {
 	public static void Main() {
-		IntPtr[] devices = FTDIContext.GetDeviceList(0x0403, 0x6001);
+		IntPtr[] devices = FTDIContext.GetDeviceList(0x0403, 0x4003);
 
 		if (devices.Length == 0) {
 			Console.WriteLine("No devices found, exiting");
@@ -13,9 +13,23 @@ class FTDITest {
 		Console.WriteLine("{0} devices found", devices.Length);
 
 		FTDIContext ftdi = new FTDIContext(devices[0]);
-
-//		FTDIContext ftdi = new FTDIContext(0x0403, 0x6001);
+		ftdi.Baudrate = 57600;
+		ftdi.PurgeBuffers();
 
 		Console.WriteLine(ftdi);
+		byte[] b;
+		int ret;
+		do {
+			b = new byte[1];
+			ret = ftdi.ReadData(b, 1);
+			Console.WriteLine("{0}", ret);
+			Console.WriteLine("{0}", b[0].ToString());
+		} while (ret > 0);
+
+		b = new byte[] { 0x1 };
+		ftdi.WriteData(b, b.Length);
+		Console.WriteLine(ftdi);
+
+		ftdi.Close();
 	}
 }
