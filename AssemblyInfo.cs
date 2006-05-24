4ce1665fa89f045620097c5736533612e76beee8
Jelmer Vernooij <jelmer@samba.org>
2006-05-24 18:37:46 +0200

    Do strong signing

---
+++ AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FTDI Library")]
+[assembly: AssemblyDescription(".NET Wrapper for libftdi")]
+[assembly: AssemblyProduct("FTDI")]
+[assembly: AssemblyCopyright("2006 Jelmer Vernooij")]
+[assembly: AssemblyKeyFile("key.snk")]
+[assembly: AssemblyVersion("0.7.0.0")]
--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ MCS = mcs
 
 all:: FTDI.dll ftdi-test.exe
 
-FTDI.dll: ftdi.cs
+FTDI.dll: ftdi.cs AssemblyInfo.cs
 	$(MCS) /unsafe /t:library /out:$@ $^
 
 ftdi-test.exe: ftdi-test.cs FTDI.dll
